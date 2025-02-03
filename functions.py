3501836007e2e479f60c804a2023bb494f2b1941
Shivam Singh <shivam@shertechinfo.com>
2025-02-03 11:58:46 +0530

    Latest changes

--- functions.py
+++ functions.py
@@ -52,5 +52,128 @@
 
 
 
-t1 = ()
-print(type(t1))
+# t1 = ()
+# print(type(t1))
+
+
+
+
+
+
+# def s1(a,b):
+#     return (a+b)
+# p = s1
+# print(p(10,20))
+
+
+
+
+
+
+# def f1():
+#     def f2():
+#         return("Hello")
+#     return f2
+# print(f1()())
+    # return f2()
+# print(f1())
+# both can work
+
+
+
+
+
+
+# # this is known as closure function
+# def f1(a):
+#     def f2():
+#         print(a)
+#     return f2()
+# f1(10)
+
+
+
+
+
+
+
+# data = 10
+# # we don't have the access to use this variable inside out function until its global variable
+# def f1():
+#     global data
+#     data = data +10
+#     return data
+# print(f1())
+
+
+
+
+
+
+
+
+
+
+
+# l1 = [10,3,4,5,90]
+
+# l2= iter(l1)
+# print(next(l2))
+
+
+
+
+
+
+
+
+# import sys
+# l1 = [i for i in range(100000)]
+# g = (i for i in range(100000))
+# print(sys.getsizeof(l1))
+# print(sys.getsizeof(g))
+
+
+
+
+
+# def f1():
+#     print("hello")
+# def f2(a):
+#     return a()
+# f2(f1)
+
+
+
+
+
+
+input1 = eval(input("Enter first input: "))
+input2 = eval(input("Enter second input: "))
+def add(x,y):
+    return x+y
+def test(a):
+    global input1
+    global input2
+    return a(input1,input2)
+print(test(add))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
