b73a755715ee37497fea044e45a23bfffe24e77b
Shivam Singh <shivam@shertechinfo.com>
2025-02-05 16:35:03 +0530

    logging

--- logging.py
+++ logging.py
@@ -1,5 +1,5 @@
 import logging
-logging.basicConfig(filename="log.txt", level=logging.WARNING,filename)
+logging.basicConfig(filename="log.txt", level=logging.WARNING)
 print("Logging Demo")
 logging.debug("This is debug message")
 logging.info("This is info message")
